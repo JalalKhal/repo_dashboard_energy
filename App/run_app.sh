f18cff2947e51eec37cec3783edf90e17a152a52
JalalKhal <101316722+JalalKhal@users.noreply.github.com>
2023-01-10 13:25:49 +0100

    Update run_app.sh

--- App/run_app.sh
+++ App/run_app.sh
@@ -1,3 +1,19 @@
 #run the main application: needed to run init.sh before run this script to load/store datas
+chmod +x ./script_cron.sh
+chmod +x get_datas.sh
+chmod +x ./energies/script_cron.sh
+chmod +x ./energies/script_mongo.sh
+chmod +x ./energies/gaz/script_mongo_get_gaz.sh
+chmod +x ./energies/gaz/script_push_sql.sh
+chmod +x ./energies/gaz_elec/script_mongo_get_gaz_elec.sh
+chmod +x ./energies/gaz_elec/script_push_sql.sh
+chmod +x ./energies/gaz_industriel/script_mongo_get_gaz_industriel.sh
+chmod +x ./energies/gaz_industriel/script_push_sql.sh
+chmod +x ./energies/elec_day/script_mongo_get_elec_day.sh
+chmod +x ./energies/elec_day/script_push_sql.sh
+chmod +x ./energies/elec_met/script_mongo_get_elec_met.sh
+chmod +x ./energies/elec_met/script_push_sql.sh
+
+chmod +x ./energies/script_sql.py #script python
 python3 ./energies/dash_main.py
 echo "----- ********* HERE is your App Url  ******************  ----- "
